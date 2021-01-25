2c8c6d1e367644a70bfde20127542c499ebca2dc
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-25 01:24:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
     valid_calls.any? do |string|
       if planeteer_calls.include?(string)
-        do string
+        string
       end
   end
 end
